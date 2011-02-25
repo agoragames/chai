90b2d6deaf4da3121b4553703cfe667244926dcc
Aaron Westendorf <aaron@agoragames.com>
2011-02-25 06:17:17 -0500

    Some changes to merge in

--- stub.py
+++ stub.py
@@ -2,8 +2,11 @@
 Implementation of stubbing
 '''
 from collections import deque
+import inspect
+import types
 
 from expectation import Expectation
+from exceptions import *
 
 def stub(obj, attr=None):
   '''
@@ -11,7 +14,44 @@ def stub(obj, attr=None):
   on the object. Only required for modules and other rare cases where we
   can't determine the binding from the object.
   '''
-  return Stub()
+  if attr:
+    return self._stub_attr(obj, attr)
+  else:
+    return self._stub_obj(obj)
+
+def _stub_attr(obj, attr_name):
+  '''
+  Stub an attribute of an object. Will return an existing stub if there already
+  is one.
+  '''
+  attr = getattr(obj, attr_name)
+  
+  # Return an existing stub
+  if isinstance(attr, Stub):
+    return attr
+
+  if isinstance(attr, property):
+    return StubProperty(obj, attr_name)
+
+  if isinstance(attr, types.MethodType):
+    return StubMethod(obj, attr_name)
+
+  raise UnsupportedStub("can't stub %s of %s", attr_name, obj)
+
+
+def _stub_obj(obj):
+  '''
+  Stub an object directly.
+  '''
+  # Return an existing stub
+  if isinstance(obj, Stub):
+    return obj
+ 
+  # can't stub properties directly because  
+  if isinstance(obj, property):
+    return StubProperty(obj)
+
+  
 
 
 class Stub(object):
@@ -19,10 +59,12 @@ class Stub(object):
   Base class for all stubs.
   '''
 
-  def __init__(self):
+  def __init__(self, obj, attr=None):
     '''
     Setup the structs for expectations
     '''
+    self._obj = obj
+    self._attr = attr
     self._expectations = deque()
     self._is_met = True
 
@@ -53,3 +95,11 @@ class Stub(object):
     for exp in self._exepctations:
       handled = exp.call(*args, **kwargs)
       if handled: break
+
+class StubProperty(Stub):
+  '''
+  Property stubbing.
+  '''
+  
+  def __init__(self, obj, attr):
+    super(Stub,self).__init__(obj, attr)

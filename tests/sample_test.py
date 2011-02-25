93349ebab0f986f03c7456cff2419062ba3d02f8
Vitaly Babiy <vbabiy@agoragames.com>
2011-02-25 11:04:33 -0500

    added test for class methods

--- tests/sample_test.py
+++ tests/sample_test.py
@@ -28,11 +28,10 @@ class SampleBaseTest(Chai):
     self.expect(obj.bound_method).args(1, 2).raises(CustomException())
     self.assertRaises(CustomException, obj.bound_method, 1, 2)
 
-  
-#  def test_expects_class_method(self):
-#    obj = SampleBase()
-#    self.expect(obj.a_classmethod).returns(12)
-#    self.assertEquals(12, obj.a_classmethod())
+  def test_expects_class_method(self):
+    obj = SampleBase()
+    self.expect(obj.a_classmethod).returns(12)
+    self.assertEquals(12, obj.a_classmethod())
 
-#    self.expect(SampleBase.a_classmethod).returns(100)
-#    self.assertEquals(100, SampleBase.a_classmethod())
+    self.expect(SampleBase.a_classmethod).returns(100)
+    self.assertEquals(100, SampleBase.a_classmethod())

b05b35448ab05f196497e17497277cdbec85f242
Aaron Westendorf <aaron@agoragames.com>
2011-02-25 11:15:24 -0500

    Lots o tests

--- tests/sample_test.py
+++ tests/sample_test.py
@@ -4,6 +4,7 @@ Tests for the sample module
 
 from chai import Chai
 from stub import stub
+from exception import *
 from samples import SampleBase, SampleChild
 
 import unittest2 as unittest
@@ -28,11 +29,62 @@ class SampleBaseTest(Chai):
     self.expect(obj.bound_method).args(1, 2).raises(CustomException())
     self.assertRaises(CustomException, obj.bound_method, 1, 2)
 
-  
-#  def test_expects_class_method(self):
-#    obj = SampleBase()
-#    self.expect(obj.a_classmethod).returns(12)
-#    self.assertEquals(12, obj.a_classmethod())
+  def test_expects_bound_method_can_be_used_for_iterative_testing(self):
+    obj = SampleBase()
+    self.expect(obj.bound_method).args(1, 2).returns(12)
+    self.assertEquals(12, obj.bound_method(1, 2))
+    self.assertRaises(UnexpectedCall, obj.bound_method)
 
-#    self.expect(SampleBase.a_classmethod).returns(100)
-#    self.assertEquals(100, SampleBase.a_classmethod())
+    self.expect(obj.bound_method).args(1, 4).returns(1100)
+    self.assertEquals(1100, obj.bound_method(1, 4))
+
+  def test_stub_bound_method_raises_unexpectedcall(self):
+    obj = SampleBase()
+    self.stub(obj.bound_method)
+    self.assertRaises(UnexpectedCall, obj.bound_method)
+
+  def test_expect_unbound_method_acts_as_any_instance(self):
+    self.expect( SampleBase.bound_method ).args('hello').returns('world')
+    self.expect( SampleBase.bound_method ).args('hello').returns('mars')
+
+    obj1 = SampleBase()
+    obj2 = SampleBase()
+    self.assertEquals( 'world', obj2.bound_method('hello') )
+    self.assertEquals( 'mars', obj1.bound_method('hello') )
+    self.assertRaises(UnexpectedCall, obj2.bound_method)
+
+  def test_stub_unbound_method_acts_as_no_instance(self):
+    self.stub( SampleBase.bound_method )
+
+    obj1 = SampleBase()
+    obj2 = SampleBase()
+    self.assertRaises(UnexpectedCall, obj2.bound_method)
+    self.assertRaises(UnexpectedCall, obj1.bound_method)
+  
+  def test_expects_class_method(self):
+    self.expect(SampleBase.a_classmethod).returns(12)
+    self.assertEquals(12, SampleBase.a_classmethod())
+
+    obj = SampleBase()
+    self.expect(SampleBase.a_classmethod).returns(100)
+    self.assertEquals(100, obj.a_classmethod())
+
+  def test_stub_class_method(self):
+    self.stub(SampleBase.a_classmethod)
+    self.assertRaises(UnexpectedCall, SampleBase.a_classmethod)
+
+    obj = SampleBase()
+    self.assertRaises(UnexpectedCall, obj.a_classmethod)
+
+  def test_expect_callback(self):
+    obj = SampleBase()
+    self.expect(obj.callback_target)
+    obj.callback_source()
+
+class SampleChildTest(Chai):
+
+  def test_stub_base_class_expect_child_classmethod(self):
+    self.stub(SampleBase.a_classmethod)
+    self.expect(SampleChild.a_classmethod)
+
+    SampleChild.a_classmethod()

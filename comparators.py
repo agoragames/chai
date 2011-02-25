f63eca29cfd6127b4fa2aa264c5f389bce26e9d1
Vitaly Babiy <vbabiy@agoragames.com>
2011-02-25 15:10:28 -0500

    A bunch of new comparators

--- comparators.py
+++ comparators.py
@@ -1,3 +1,5 @@
+import re
+
 '''
 All of the comparators that can be used for arguments.
 '''
@@ -29,6 +31,50 @@ class InstanceOf(Comparator):
   def test(self, value):
     return isinstance(value, self._types)
 
+class Is(Comparator):
+  '''
+  Checks for identity not equality
+  '''
+  def __init__(self, obj):
+    self._obj = obj
+    
+  def test(self, value):
+    return self._obj is value
+
+class AlmostEqual(Comparator):
+  '''
+  Compare a float value to n number of palces
+  '''
+  
+  def __init__(self, float_value, places=7):
+    self._float_value = float_value
+    self._places = places
+  
+  def test(self, value):
+    return round(value - self._float_value, self._places) == 0
+
+class StringContains(Comparator):
+  '''
+  Check to see if a str in with another string.
+  '''
+  
+  def __init__(self, search_string):  
+    self._search_string = search_string
+  
+  def test(self, value):
+    return self._search_string.find(value) > -1
+
+class Regex(Comparator):
+  '''
+  Checks to see if a string matches a regex
+  '''
+  
+  def __init__(self, pattern, flags=0):
+    self._regex = re.complie(pattern)
+  
+  def test(self, value):
+    return self._regex.search(value) is not None
+
 class Any(Comparator):
   '''
   Test to see if any comparator matches
@@ -40,6 +86,16 @@ class Any(Comparator):
     for comp in self._comparators:
       if comp(value): return True
     return False
+
+class In(Comparator):
+  '''
+  Test if a key is in a list or dict
+  '''
+  def __init__(self, key):
+    self._key = key
+
+  def test(self, value):
+    return self._key in value
 
 class All(Comparator):
   '''
@@ -63,3 +119,19 @@ class Not(Comparator):
   def test(self, value):
     return not self._comparator.test(value)
 
+class Function(Comparator):
+  '''
+  Call a func to compare the values
+  '''
+  def __init__(self, func):
+    self._func = func
+
+  def test(self, value):
+    return self._func(value)
+
+class Ignore(Comparator):
+  '''
+  Igore this argument
+  '''
+  def test(self, value):
+    return True

8705a3fdf543fc0eb469d4e466f0cd4b12ff7bce
Vitaly Babiy <vbabiy@agoragames.com>
2011-02-25 15:51:06 -0500

    no more cAmElCaSe

--- tests/sample_test.py
+++ tests/sample_test.py
@@ -17,71 +17,71 @@ class SampleBaseTest(Chai):
   def test_expects_bound_method_returns(self):
     obj = SampleBase()
     self.expect(obj.bound_method).args(1, 2).returns(12)
-    self.assertEquals(12, obj.bound_method(1, 2))
+    self.assert_equals(12, obj.bound_method(1, 2))
 
     self.expect(obj.bound_method).args(1, 4).returns(1100)
-    self.assertEquals(1100, obj.bound_method(1, 4))
+    self.assert_equals(1100, obj.bound_method(1, 4))
 
   def test_expects_bound_method_at_least_with_other_expectation(self):
     obj = SampleBase()
     self.expect(obj.bound_method).args(1, 2).returns(12).at_least(3)
-    self.assertEquals(12, obj.bound_method(1, 2))
-    self.assertEquals(12, obj.bound_method(1, 2))
-    self.assertEquals(12, obj.bound_method(1, 2))
-    self.assertEquals(12, obj.bound_method(1, 2))
+    self.assert_equals(12, obj.bound_method(1, 2))
+    self.assert_equals(12, obj.bound_method(1, 2))
+    self.assert_equals(12, obj.bound_method(1, 2))
+    self.assert_equals(12, obj.bound_method(1, 2))
     
     self.expect(obj.bound_method).args(1, 3).returns(100)
-    self.assertEquals(100, obj.bound_method(1, 3))
+    self.assert_equals(100, obj.bound_method(1, 3))
 
   def test_expects_bound_method_at_least_as_last_expectation(self):
     obj = SampleBase()
     self.expect(obj.bound_method).args(1, 2).returns(12).at_least(3)
-    self.assertEquals(12, obj.bound_method(1, 2))
-    self.assertEquals(12, obj.bound_method(1, 2))
-    self.assertEquals(12, obj.bound_method(1, 2))
-    self.assertEquals(12, obj.bound_method(1, 2))
+    self.assert_equals(12, obj.bound_method(1, 2))
+    self.assert_equals(12, obj.bound_method(1, 2))
+    self.assert_equals(12, obj.bound_method(1, 2))
+    self.assert_equals(12, obj.bound_method(1, 2))
 
   def test_expects_bound_method_at_most(self):
     obj = SampleBase()
     self.expect(obj.bound_method).args(1, 2).returns(12).at_most(3)
-    self.assertEquals(12, obj.bound_method(1, 2))
-    self.assertEquals(12, obj.bound_method(1, 2))
+    self.assert_equals(12, obj.bound_method(1, 2))
+    self.assert_equals(12, obj.bound_method(1, 2))
     obj.bound_method(1, 2)
-    self.assertRaises(UnexpectedCall, obj.bound_method, 1, 2)
+    self.assert_raises(UnexpectedCall, obj.bound_method, 1, 2)
 
   def test_expects_bound_method_raises(self):
     obj = SampleBase()
     self.expect(obj.bound_method).args(1, 2).raises(CustomException)
-    self.assertRaises(CustomException, obj.bound_method, 1, 2)
+    self.assert_raises(CustomException, obj.bound_method, 1, 2)
 
     self.expect(obj.bound_method).args(1, 2).raises(CustomException())
-    self.assertRaises(CustomException, obj.bound_method, 1, 2)
+    self.assert_raises(CustomException, obj.bound_method, 1, 2)
 
   def test_expects_bound_method_can_be_used_for_iterative_testing(self):
     obj = SampleBase()
     self.expect(obj.bound_method).args(1, 2).returns(12)
-    self.assertEquals(12, obj.bound_method(1, 2))
-    self.assertRaises(UnexpectedCall, obj.bound_method)
+    self.assert_equals(12, obj.bound_method(1, 2))
+    self.assert_raises(UnexpectedCall, obj.bound_method)
 
     self.expect(obj.bound_method).args(1, 4).returns(1100)
-    self.assertEquals(1100, obj.bound_method(1, 4))
+    self.assert_equals(1100, obj.bound_method(1, 4))
 
   def test_stub_bound_method_raises_unexpectedcall(self):
     obj = SampleBase()
     self.stub(obj.bound_method)
-    self.assertRaises(UnexpectedCall, obj.bound_method)
+    self.assert_raises(UnexpectedCall, obj.bound_method)
   
   def test_expect_bound_method_with_equals_comparator(self):
     obj = SampleBase()
     self.expect(obj.bound_method).args( self.equals(42) )
     obj.bound_method( 42 )
-    self.assertRaises(UnexpectedCall, obj.bound_method, 32 )
+    self.assert_raises(UnexpectedCall, obj.bound_method, 32 )
 
   def test_expect_bound_method_with_instanceof_comparator(self):
     obj = SampleBase()
     self.expect(obj.bound_method).args( self.instance_of(int) )
     obj.bound_method( 42 )
-    self.assertRaises(UnexpectedCall, obj.bound_method, '42' )
+    self.assert_raises(UnexpectedCall, obj.bound_method, '42' )
   
   def test_expect_bound_method_with_anyof_comparator(self):
     obj = SampleBase()
@@ -91,7 +91,7 @@ class SampleBaseTest(Chai):
     obj.bound_method( 3.14 )
     obj.bound_method( 'hello' )
     obj.bound_method( [1,2,3] )
-    self.assertRaises(UnexpectedCall, obj.bound_method, '42' )
+    self.assert_raises(UnexpectedCall, obj.bound_method, '42' )
 
   def test_expect_bound_method_with_allof_comparator(self):
     obj = SampleBase()
@@ -109,32 +109,32 @@ class SampleBaseTest(Chai):
 
     obj1 = SampleBase()
     obj2 = SampleBase()
-    self.assertEquals( 'world', obj2.bound_method('hello') )
-    self.assertEquals( 'mars', obj1.bound_method('hello') )
-    self.assertRaises(UnexpectedCall, obj2.bound_method)
+    self.assert_equals( 'world', obj2.bound_method('hello') )
+    self.assert_equals( 'mars', obj1.bound_method('hello') )
+    self.assert_raises(UnexpectedCall, obj2.bound_method)
 
   def test_stub_unbound_method_acts_as_no_instance(self):
     self.stub( SampleBase.bound_method )
 
     obj1 = SampleBase()
     obj2 = SampleBase()
-    self.assertRaises(UnexpectedCall, obj2.bound_method)
-    self.assertRaises(UnexpectedCall, obj1.bound_method)
+    self.assert_raises(UnexpectedCall, obj2.bound_method)
+    self.assert_raises(UnexpectedCall, obj1.bound_method)
   
   def test_expects_class_method(self):
     self.expect(SampleBase.a_classmethod).returns(12)
-    self.assertEquals(12, SampleBase.a_classmethod())
+    self.assert_equals(12, SampleBase.a_classmethod())
 
     obj = SampleBase()
     self.expect(SampleBase.a_classmethod).returns(100)
-    self.assertEquals(100, obj.a_classmethod())
+    self.assert_equals(100, obj.a_classmethod())
 
   def test_stub_class_method(self):
     self.stub(SampleBase.a_classmethod)
-    self.assertRaises(UnexpectedCall, SampleBase.a_classmethod)
+    self.assert_raises(UnexpectedCall, SampleBase.a_classmethod)
 
     obj = SampleBase()
-    self.assertRaises(UnexpectedCall, obj.a_classmethod)
+    self.assert_raises(UnexpectedCall, obj.a_classmethod)
 
   def test_expect_callback(self):
     obj = SampleBase()
@@ -159,44 +159,44 @@ class SampleBaseTest(Chai):
   def test_regex_comaptor(self):
     obj = SampleBase()
     self.expect(obj.bound_method).args(self.matches("name$")).returns(100)
-    self.assertEquals(obj.bound_method('first_name'), 100)
+    self.assert_equals(obj.bound_method('first_name'), 100)
 
   def test_regex_comaptor(self):
     obj = SampleBase()
     self.expect(obj.bound_method).args(self.matches("name$")).returns(100)
-    self.assertEquals(obj.bound_method('first_name'), 100)
+    self.assert_equals(obj.bound_method('first_name'), 100)
 
   def test_ignore_arg(self):
     obj = SampleBase()
     self.expect(obj.bound_method).args(self.ignore_arg()).returns(100)
-    self.assertEquals(obj.bound_method('first_name'), 100)
+    self.assert_equals(obj.bound_method('first_name'), 100)
 
   def test_function_comaptor(self):
     obj = SampleBase()
     self.expect(obj.bound_method).args(self.use_func(lambda arg: arg > 10)).returns(100)
-    self.assertEquals(obj.bound_method(100), 100)
+    self.assert_equals(obj.bound_method(100), 100)
 
   def test_in_comaptor(self):
     obj = SampleBase()
     self.expect(obj.bound_method).args(self.in_arg('name')).returns(100).at_most(3)
-    self.assertEquals(obj.bound_method(['name', 'age']), 100)
-    self.assertEquals(obj.bound_method({'name' : 'vitaly'}), 100)
-    self.assertEquals(obj.bound_method('lasfs-name-asfsad'), 100)
+    self.assert_equals(obj.bound_method(['name', 'age']), 100)
+    self.assert_equals(obj.bound_method({'name' : 'vitaly'}), 100)
+    self.assert_equals(obj.bound_method('lasfs-name-asfsad'), 100)
 
   def test_contains_comaptor(self):
     obj = SampleBase()
     self.expect(obj.bound_method).args(self.contains('name')).returns(100)
-    self.assertEquals(obj.bound_method('lasfs-name-asfsad'), 100)
+    self.assert_equals(obj.bound_method('lasfs-name-asfsad'), 100)
 
   def test_almost_equals_comaptor(self):
     obj = SampleBase()
     self.expect(obj.bound_method).args(self.almost_equals(10.1234, 2)).returns(100)
-    self.assertEquals(obj.bound_method(10.12), 100)
+    self.assert_equals(obj.bound_method(10.12), 100)
 
   def test_is_comaptor(self):
     obj = SampleBase()
     self.expect(obj.bound_method).args(self.is_arg(obj)).returns(100)
-    self.assertEquals(obj.bound_method(obj), 100)
+    self.assert_equals(obj.bound_method(obj), 100)
 
 class SampleChildTest(Chai):
 

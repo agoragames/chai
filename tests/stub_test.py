6bed90d985b2d0c8c212e61aedf5a91d3b3c3cc4
Aaron Westendorf <aaron@agoragames.com>
2011-02-25 07:22:51 -0500

    Changed name of exceptions due to pain with core exceptions.py module

--- tests/stub_test.py
+++ tests/stub_test.py
@@ -5,9 +5,14 @@ from stub import *
 
 class StubTest(unittest.TestCase):
 
+  ###
+  ### Test the public stub() method
+  ###
   def test_stub_property(self):
     class Foo(object):
       @property
       def prop(self): return 3
 
-    
+    res = stub(Foo, 'prop')
+    self.assertTrue( isinstance(res,StubProperty) )
+    self.assertEquals( res, stub(Foo,'prop') )

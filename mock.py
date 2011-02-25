cb3f2069d6a03ec51fc07d7ce9c363712099a56b
Aaron Westendorf <aaron@agoragames.com>
2011-02-25 13:17:34 -0500

    Changed Chai.mock() so that it can be used to install a Mock object on an attribute. Updated Chai.teardown() so it cleans up mocks Lots of changes to Mock to support __call__ and clean up __getattr__ Added StubMethodWrapper for another weird edge case Added examples of mocking an instance and module attribute

--- mock.py
+++ mock.py
@@ -2,20 +2,31 @@
 An open mocking object.
 '''
 from types import MethodType
-from stub import stub
+from stub import stub, Stub
 
 class Mock(object):
   '''
   A class where all calls are stubbed.
   '''
+  # For whatever reason, new-style objects require this method defined before
+  # any instance is created. Defining it through __getattr__ is not enough. This
+  # appears to be a bug/feature in new classes where special members, or at least
+  # __call__, have to defined when the instance is created. Also, if it's already
+  # defined on the instance, getattr() will return the stub but the original
+  # method will always be called. Anyway, it's all crazy, but that's why the 
+  # implementation of __call__ is so weird.
+  def __call__(self, *args, **kwargs):
+    if isinstance(getattr(self,'__call__'), Stub):
+      return getattr(self,'__call__')(*args, **kwargs)
+
   def __getattr__(self,name):
     rval = self.__dict__.get(name)
 
-    if not rval:
+    if not rval or not isinstance(rval,Stub):
       def noop(*args, **kwargs): pass
       noop.func_name = name
 
-      setattr(self, name, MethodType(noop, self, Mock) ) 
-      rval = stub( self, name )
+      rval = MethodType(noop, self, Mock)
+      setattr(self, name, rval)
 
     return rval

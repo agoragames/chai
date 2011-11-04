e729cfd706c9ee73a8cc1eae63d7a95c75a234ee
Aaron Westendorf <aaron@agoragames.com>
2011-11-04 10:20:00 -0400

    chai 0.1.17

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ requirements = [r for r in map(str.strip, open('requirements.txt').readlines())]
 
 setup(
     name='chai',
-    version='0.1.16',
+    version='0.1.17',
     author='Vitaly Babiy, Aaron Westendorf',
     author_email="vbabiy@agoragames.com, aaron@agoragames.com",
     packages=['chai'],

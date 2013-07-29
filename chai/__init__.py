85db11776e21b6444afca5e217042c69f847eb75
Aaron Westendorf <aaron@agoragames.com>
2013-07-29 17:15:35 -0400

    chai 0.3.7

--- chai/__init__.py
+++ chai/__init__.py
@@ -4,5 +4,5 @@ Copyright (c) 2011-2013, Agora Games, LLC All rights reserved.
 https://github.com/agoragames/chai/blob/master/LICENSE.txt
 '''
 
-__version__ = '0.3.6'
+__version__ = '0.3.7'
 from chai import Chai
